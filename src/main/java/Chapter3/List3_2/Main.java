a03626ef00e1de824d223842437547f45a65a8f4
General0917 <ht13a037@oecu.jp>
2024-06-02 05:13:13 +0900

    3.3の学習完了

---
+++ src/main/java/Chapter3/List3_2/Main.java
@@ -0,0 +1,11 @@
+package Chapter3.List3_2;
+
+public class Main {
+    public static void main(String args[]) {
+        boolean doorClass = true;
+        while (doorClass == true) {
+            System.out.println("ノックする");
+            System.out.println("1分待つ");
+        }
+    }
+}
---
+++ src/main/java/Chapter3/List3_3/Main.java
@@ -0,0 +1,12 @@
+package Chapter3.List3_3;
+
+public class Main {
+    public static void main(String args[]) {
+        boolean tenki = true;
+        if (tenki == true) {
+            System.out.println("選択をします");
+            System.out.println("散歩に行きます");
+        } else
+            System.out.println("DVDを見ます");
+    }
+}
